eaadd256af4a241f3116d8f9331663b75c3eefa2
Ilya Dombrovsky <61749139+ilyaDombrovsky@users.noreply.github.com>
2022-07-08 08:59:42 +0300

    Initial Commit

---
+++ FinancialAccounting/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FinancialAccounting
+//
+//  Created by Ilya Dombrovsky on 8.07.22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
